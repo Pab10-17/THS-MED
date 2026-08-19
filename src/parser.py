95048176955157bc8bf042ff477b0831ded21b30
Pablo <email.ismailq@gmail.com>
2026-08-19 04:03:30 +0100

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,16 +1,34 @@
+import re
 from bs4 import BeautifulSoup
 
 
-def get_meta_content(soup: BeautifulSoup, attribute: str, value: str):
+def get_meta_content(soup, attribute, value):
     tag = soup.find("meta", attrs={attribute: value})
-    if tag:
-        return tag.get("content")
-    return None
+    return tag.get("content") if tag else None
 
 
 def parse_event(soup):
+
+    title = get_meta_content(soup, "property", "og:title")
+    description = get_meta_content(soup, "property", "og:description")
+    image = get_meta_content(soup, "property", "og:image")
+
+    dates = []
+
+    for tag in soup.select(".w-key-info__date"):
+        date = tag.get_text(" ", strip=True)
+
+        if date not in dates:
+            dates.append(date)
+
+    year_match = re.search(r"20\d\d", description or "")
+    year = year_match.group(0) if year_match else ""
+
+    full_dates = [f"{d} {year}" for d in dates]
+
     return {
-        "title": get_meta_content(soup, "property", "og:title"),
-        "description": get_meta_content(soup, "property", "og:description"),
-        "image": get_meta_content(soup, "property", "og:image"),
+        "title": title,
+        "description": description,
+        "image": image,
+        "dates": full_dates,
     }
