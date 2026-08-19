a2fe41c74d6ff40a955d054a12bc7e0d010a49f2
Pablo <email.ismailq@gmail.com>
2026-08-19 06:09:11 +0100

    Update calendar_builder.py

--- src/calendar_builder.py
+++ src/calendar_builder.py
@@ -4,13 +4,30 @@ from icalendar import Calendar, Event
 import hashlib
 
 
+def get_icon(title):
+    t = title.lower()
+
+    if "spurs" in t:
+        return "⚽"
+
+    if "nfl" in t:
+        return "🏈"
+
+    if "saracens" in t:
+        return "🏉"
+
+    if "tyson" in t or "boxing" in t:
+        return "🥊"
+
+    return "🎤"
+
+
 def build_calendar(events):
     cal = Calendar()
 
     cal.add("prodid", "-//THS-MED//EN")
     cal.add("version", "2.0")
 
-    # Sort by start date
     events.sort(key=lambda e: parse(e["start"]))
 
     for item in events:
@@ -24,8 +41,9 @@ def build_calendar(events):
         ).hexdigest() + "@ths-med"
 
         event.add("uid", uid)
-        event.add("summary", item["title"])
+        event.add("summary", f"{get_icon(item['title'])} {item['title']}")
         event.add("description", item["description"])
+        event.add("location", "Tottenham Hotspur Stadium")
         event.add("url", item["url"])
         event.add("dtstart", start)
         event.add("dtend", end)
