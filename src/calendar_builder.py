aff8475d8c4443095536dd49a39db6f11e579123
Pablo <email.ismailq@gmail.com>
2026-08-19 06:03:44 +0100

    Update calendar_builder.py

--- src/calendar_builder.py
+++ src/calendar_builder.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from dateutil.parser import isoparse
+from dateutil.parser import parse
 from icalendar import Calendar, Event
+import hashlib
 
 
 def build_calendar(events):
@@ -9,24 +10,28 @@ def build_calendar(events):
     cal.add("prodid", "-//THS-MED//EN")
     cal.add("version", "2.0")
 
+    # Sort by date
+    events.sort(key=lambda e: parse(e["dates"][0], dayfirst=True))
+
     for item in events:
+        for date in item["dates"]:
+            event = Event()
 
-        event = Event()
+            dt = parse(date, dayfirst=True)
 
-        event.add("summary", item["title"])
-        event.add("description", item["description"])
+            # Stable UID
+            uid = hashlib.md5(
+                f"{item['title']}-{date}".encode("utf-8")
+            ).hexdigest() + "@ths-med"
 
-        if item.get("url"):
+            event.add("uid", uid)
+            event.add("summary", item["title"])
+            event.add("description", item["description"])
             event.add("url", item["url"])
+            event.add("dtstart", dt.date())
+            event.add("dtend", dt.date())
+            event.add("dtstamp", datetime.utcnow())
 
-        start = isoparse(item["start"])
-        end = isoparse(item["end"])
-
-        event.add("dtstart", start)
-        event.add("dtend", end)
-
-        event.add("dtstamp", datetime.utcnow())
-
-        cal.add_component(event)
+            cal.add_component(event)
 
     return cal
