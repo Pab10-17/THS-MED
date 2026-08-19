45a844d83019749f10422b76fdc1e1dc315914b6
Pablo <email.ismailq@gmail.com>
2026-08-19 03:27:56 +0100

    Update update_calendar.py

--- update_calendar.py
+++ update_calendar.py
@@ -1,1 +1,16 @@
-print("THS-MED updater is running!")
+import requests
+
+url = "https://www.tottenhamhotspurstadium.com/events"
+
+print("Downloading:", url)
+
+response = requests.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
+    timeout=30
+)
+
+print("Status Code:", response.status_code)
+print("Downloaded", len(response.text), "characters")
